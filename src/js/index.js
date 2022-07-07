fc72990b9acc5e57eb7cc5cffc6cd498ede66d84
Vladyslav Avidzba <wasssd1@gmail.com>
2022-07-07 22:34:46 +0300

    finaly-redundent

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,6 @@ import './components/header-line';
 import './components/burger-line';
 import './components/mobile-menu';
 import './components/gallery';
-import './components/footer-modal';
 import './firebase/authentication';
 import './components/authentication-modal';
 import './components/pagination-bar';
